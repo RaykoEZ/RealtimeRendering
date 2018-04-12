7041b677709aa6c6e5f751b310001ef3cd32cb8e
RaykoEZ <onion55zh@gmail.com>
2018-04-12 20:34:58 +0100

    setting up sdf, need debugging

--- README.md
+++
@@ -1,2 +0,0 @@
-# RealtimeRendering
-This repo is for the assignment of SDAGE L5 Real-time rendering class.
---
+++ src/NGLScene.cpp
@@ -0,0 +1,317 @@
+#include "NGLScene.h"
+#include <QGuiApplication>
+#include <QMouseEvent>
+
+#include <ngl/Camera.h>
+#include <ngl/Light.h>
+#include <ngl/Material.h>
+#include <ngl/NGLInit.h>
+#include <ngl/NGLStream.h>
+#include <ngl/ShaderLib.h>
+#include <ngl/VAOPrimitives.h>
+#include <ngl/Image.h>
+
+NGLScene::NGLScene()
+{
+  setTitle( "Qt5 Simple NGL Demo" );
+  m_startTime = std::chrono::high_resolution_clock::now();
+  m_eye = ngl::Vec3(0.0, 0.0, 2.0);
+  m_lookAt = ngl::Vec3(0.0, 0.0, 0.0);
+}
+
+
+NGLScene::~NGLScene()
+{
+  std::cout << "Shutting down NGL, removing VAO's and Shaders\n";
+}
+
+
+
+void NGLScene::resizeGL( int _w, int _h )
+{
+  m_cam.setShape( 45.0f, static_cast<float>( _w ) / _h, 0.05f, 350.0f );
+  m_win.width  = static_cast<int>( _w * devicePixelRatio() );
+  m_win.height = static_cast<int>( _h * devicePixelRatio() );
+}
+
+
+void NGLScene::initializeGL()
+{
+  // we must call that first before any other GL commands to load and link the
+  // gl commands from the lib, if that is not done program will crash
+  ngl::NGLInit::instance();
+
+  glClearColor( 0.4f, 0.4f, 0.4f, 1.0f ); // Grey Background
+  // enable depth testing for drawing
+  glEnable( GL_DEPTH_TEST );
+// enable multisampling for smoother drawing
+
+  glEnable( GL_MULTISAMPLE );
+
+  // now to load the shader and set the values
+  // grab an instance of shader manager
+  ngl::ShaderLib* shader = ngl::ShaderLib::instance();
+  // we are creating a shader called Phong to save typos
+  // in the code create some constexpr
+  constexpr auto shaderProgram = "Marbles";
+  constexpr auto vertexShader  = "MarblesVertex";
+  constexpr auto fragShader    = "MarblesFragment";
+  // create the shader program
+  shader->createShaderProgram( shaderProgram );
+  // now we are going to create empty shaders for Frag and Vert
+  shader->attachShader( vertexShader, ngl::ShaderType::VERTEX );
+  shader->attachShader( fragShader, ngl::ShaderType::FRAGMENT );
+  // attach the source
+  shader->loadShaderSource( vertexShader, "shaders/MarblesVert.glsl" );
+  shader->loadShaderSource( fragShader, "shaders/MarblesFragment.glsl" );
+  // compile the shaders
+  shader->compileShader( vertexShader );
+  shader->compileShader( fragShader );
+  // add them to the program
+  shader->attachShaderToProgram( shaderProgram, vertexShader );
+  shader->attachShaderToProgram( shaderProgram, fragShader );
+
+  //initEnvironment();
+  //initTexture(1, m_glossMapTex, "images/gloss.png");
+
+  // now we have associated that data we can link the shader
+  shader->linkProgramObject( shaderProgram );
+  // and make it active ready to load values
+  ( *shader )[ shaderProgram ]->use();
+  shader->setUniform("glossMap", 1);
+  // the shader will use the currently active material and light0 so set them
+  //ngl::Material m( ngl::STDMAT::GOLD );
+  // load our material values to the shader into the structure material (see Vertex shader)
+  //m.loadToShader( "material" );
+  // Now we will create a basic Camera from the graphics library
+  // This is a static camera so it only needs to be set once
+  // First create Values for the camera position
+  ngl::Vec3 from( 0, 10, 15 );
+  ngl::Vec3 to( 0, 1, 0 );
+  ngl::Vec3 up( 0, 1, 0 );
+  // now load to our new camera
+  m_cam.set( from, to, up );
+  // set the shape using FOV 45 Aspect Ratio based on Width and Height
+  // The final two are near and far clipping planes of 0.5 and 10
+  m_cam.setShape( 45.0f, 720.0f / 576.0f, 0.05f, 350.0f );
+  shader->setUniform( "viewerPos", m_cam.getEye().toVec3() );
+  // now create our light that is done after the camera so we can pass the
+  // transpose of the projection matrix to the light to do correct eye space
+  // transformations
+  //ngl::Mat4 iv = m_cam.getViewMatrix();
+
+  //iv.transpose();
+  //ngl::Light light( ngl::Vec3( -2, 5, 2 ), ngl::Colour( 1, 1, 1, 1 ), ngl::Colour( 1, 1, 1, 1 ), ngl::LightModes::POINTLIGHT );
+  //light.setTransform( iv );
+
+  // prepare mesh by loading from obj
+  //m_mesh.reset(  new ngl::Obj(m_objPath));
+  //m_mesh->createVAO();
+  //glViewport(0,0,width(),height());
+  // load these values to the shader as well
+  //light.loadToShader( "light" );
+
+  ngl::VAOPrimitives *prim=ngl::VAOPrimitives::instance();
+  prim->createTrianglePlane("plane",10,10,5,5,ngl::Vec3(0,1,0));
+}
+
+
+void NGLScene::loadMatricesToShader()
+{
+  ngl::ShaderLib* shader = ngl::ShaderLib::instance();
+  // Calculate the elapsed time since the programme started
+  auto now = std::chrono::high_resolution_clock::now();
+  double t = std::chrono::duration_cast<std::chrono::milliseconds>(now - m_startTime).count()  * 0.001;
+  ngl::Mat4 MV;
+  ngl::Mat4 MVP;
+  ngl::Mat3 N;
+  ngl::Mat4 M;
+  M            = m_mouseGlobalTX;
+  MV           = m_cam.getViewMatrix() * M;
+  MVP          = m_cam.getVPMatrix() * M;
+  //alligning plane to screen
+  MVP.rotateX(90.0);
+  N = MV;
+  N.inverse().transpose();
+  shader->setUniform( "MV", MV );
+  shader->setUniform( "MVP", MVP );
+  shader->setUniform( "N", N );
+  shader->setUniform( "M", M );
+  //For sdf, we need these uniforms
+  shader->setUniform("resolution",ngl::Vec3(1.0f,1.0f,0.0f));
+  shader->setUniform("time", float(t));
+  shader->setUniform("eyePos",ngl::Vec3(m_eye));
+  shader->setUniform("lookAt",ngl::Vec3(m_lookAt));
+}
+
+void NGLScene::paintGL()
+{
+  glViewport( 0, 0, m_win.width, m_win.height );
+  // clear the screen and depth buffer
+  glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+  // grab an instance of the shader manager
+  ngl::ShaderLib* shader = ngl::ShaderLib::instance();
+  ( *shader )[ "Marbles" ]->use();
+
+  // Rotation based on the mouse position for our global transform
+  ngl::Mat4 rotX;
+  ngl::Mat4 rotY;
+  // create the rotation matrices
+  rotX.rotateX( m_win.spinXFace );
+  rotY.rotateY( m_win.spinYFace );
+  // multiply the rotations
+  m_mouseGlobalTX = rotX * rotY;
+  // add the translations
+  m_mouseGlobalTX.m_m[ 3 ][ 0 ] = m_modelPos.m_x;
+  m_mouseGlobalTX.m_m[ 3 ][ 1 ] = m_modelPos.m_y;
+  m_mouseGlobalTX.m_m[ 3 ][ 2 ] = m_modelPos.m_z;
+
+
+  loadMatricesToShader();
+  // get the VBO instance and draw the built in teapot
+  ngl::VAOPrimitives* prim = ngl::VAOPrimitives::instance();
+  //prim->draw( "teapot" );
+  prim->draw("plane");
+  //m_mesh->draw();
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+void NGLScene::keyPressEvent( QKeyEvent* _event )
+{
+  // that method is called every time the main window recives a key event.
+  // we then switch on the key value and set the camera in the GLWindow
+  switch ( _event->key() )
+  {
+    // escape key to quit
+    case Qt::Key_Escape:
+      QGuiApplication::exit( EXIT_SUCCESS );
+      break;
+// turn on wirframe rendering
+#ifndef USINGIOS_
+    case Qt::Key_W:
+      glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+      break;
+    // turn off wire frame
+    case Qt::Key_S:
+      glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
+      break;
+#endif
+    // show full screen
+    case Qt::Key_F:
+      showFullScreen();
+      break;
+    // show windowed
+    case Qt::Key_N:
+      showNormal();
+      break;
+    case Qt::Key_Space :
+      m_win.spinXFace=0;
+      m_win.spinYFace=0;
+      m_modelPos.set(ngl::Vec3::zero());
+    break;
+    default:
+      break;
+  }
+  update();
+}
+
+void NGLScene::initTexture(const GLuint& texUnit, GLuint &texId, const char *filename) {
+    // Set our active texture unit
+    glActiveTexture(GL_TEXTURE0 + texUnit);
+
+    // Load up the image using NGL routine
+    ngl::Image img(filename);
+
+    // Create storage for our new texture
+    glGenTextures(1, &texId);
+
+    // Bind the current texture
+    glBindTexture(GL_TEXTURE_2D, texId);
+
+    // Transfer image data onto the GPU using the teximage2D call
+    glTexImage2D (
+                GL_TEXTURE_2D,    // The target (in this case, which side of the cube)
+                0,                // Level of mipmap to load
+                img.format(),     // Internal format (number of colour components)
+                img.width(),      // Width in pixels
+                img.height(),     // Height in pixels
+                0,                // Border
+                GL_RGB,          // Format of the pixel data
+                GL_UNSIGNED_BYTE, // Data type of pixel data
+                img.getPixels()); // Pointer to image data in memory
+
+    // Set up parameters for our texture
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+}
+
+void NGLScene::initEnvironment() {
+    // Enable seamless cube mapping
+    glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
+
+    // Placing our environment map texture in texture unit 0
+    glActiveTexture (GL_TEXTURE0);
+
+    // Generate storage and a reference for our environment map texture
+    glGenTextures (1, &m_envTex);
+
+    // Bind this texture to the active texture unit
+    glBindTexture(GL_TEXTURE_CUBE_MAP, m_envTex);
+
+    // Now load up the sides of the cube
+    initEnvironmentSide(GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, "images/sky_zneg.png");
+    initEnvironmentSide(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, "images/sky_zpos.png");
+    initEnvironmentSide(GL_TEXTURE_CUBE_MAP_POSITIVE_Y, "images/sky_ypos.png");
+    initEnvironmentSide(GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, "images/sky_yneg.png");
+    initEnvironmentSide(GL_TEXTURE_CUBE_MAP_NEGATIVE_X, "images/sky_xneg.png");
+    initEnvironmentSide(GL_TEXTURE_CUBE_MAP_POSITIVE_X, "images/sky_xpos.png");
+
+    // Generate mipmap levels
+    glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
+
+    // Set the texture parameters for the cube map
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_GENERATE_MIPMAP, GL_TRUE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    GLfloat anisotropy;
+    glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &anisotropy);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAX_ANISOTROPY_EXT, anisotropy);
+
+    // Set our cube map texture to on the shader so we can use it
+    ngl::ShaderLib *shader=ngl::ShaderLib::instance();
+    shader->use("EnvironmentProgram");
+    shader->setUniform("envMap", 0);
+}
+
+
+/**
+ * @brief Scene::initEnvironmentSide
+ * @param texture
+ * @param target
+ * @param filename
+ * This function should only be called when we have the environment texture bound already
+ * copy image data into 'target' side of cube map
+ */
+void NGLScene::initEnvironmentSide(GLenum target, const char *filename) {
+    // Load up the image using NGL routine
+    ngl::Image img(filename);
+
+    // Transfer image data onto the GPU using the teximage2D call
+    glTexImage2D (
+      target,           // The target (in this case, which side of the cube)
+      0,                // Level of mipmap to load
+      img.format(),     // Internal format (number of colour components)
+      img.width(),      // Width in pixels
+      img.height(),     // Height in pixels
+      0,                // Border
+      GL_RGBA,          // Format of the pixel data
+      GL_UNSIGNED_BYTE, // Data type of pixel data
+      img.getPixels()   // Pointer to image data in memory
+    );
+}
