3b17c451818ac9c475b15aeca6c8ecee3baabf95
EricZ <onion55zh@gmail.com>
2018-05-04 15:42:37 +0100

    Fixing internal shadow in refraction, almost done, gonna do caustic shadow later

--- shaders/sdf_frag.glsl
+++ shaders/sdf_frag.glsl
@@ -178,7 +178,7 @@ void setShape(float index, out vec3 shapePos, out mat3 shapeDir) {
     float t = tau * mod(index * 0.2 + 0.02 * iTime + 0.12, 1.0);
     float a = 0.0;
     float b = 3.0 ;
-    float c = 7.0*t ;
+    float c = 7.0 ;
     shapePos = vec3(1.8 * cos(b),  1.0 + sin(a), 1.8 * cos(c));
     shapeDir = mat3(cos(a), -sin(a), 0.0, sin(a), cos(a), 0.0, 0.0, 0.0, 1.0);
     shapeDir *= mat3(cos(b), 0.0, -sin(b), 0.0, 1.0, 0.0, sin(b), 0.0, cos(b));
@@ -273,10 +273,10 @@ float sceneWithoutGround(vec3 p) {
   * quite generic and was originally taken from (and explained rather well) here:
   * http://jamie-wong.com/2016/07/15/ray-marching-signed-distance-functions/
   */
-float depthMarch(vec3 eye, vec3 dir) {
+float depthMarchOut(vec3 eye, vec3 dir) {
     float depth = 0.0;
     for (int i = 0; i < marchIter; ++i) {
-        float dist = sceneOut(eye + depth * dir).x;
+        float dist = 0.5*sceneOut(eye + depth * dir).x;
         depth += dist;
         if (dist < epsilon || depth >= marchDist)
                         break;
@@ -284,6 +284,16 @@ float depthMarch(vec3 eye, vec3 dir) {
     return depth;
 }
 
+float depthMarchIn(vec3 eye, vec3 dir) {
+    float depth = 0.0;
+    for (int i = 0; i < marchIter; ++i) {
+        float dist = 0.5*sceneIn(eye + depth * dir).x;
+        depth += dist;
+        if (dist < epsilon || depth >= marchDist)
+                        break;
+    }
+    return depth;
+}
 
 
 CP findIntersection(inout vec3 p, inout vec3 rd) {
@@ -344,7 +354,7 @@ vec3 refractCaustic(vec3 p, vec3 rd, vec3 ld, inout float eta) {
 
         vec3 normal = sign(dot(rd, cp.normal))*cp.normal;
         float shadow = shadowFactor(p,normal);
-        cl *= SURFACE_COLOR;//*(abs(dot(rd, cp.normal)));
+        //cl *= mix(SURFACE_COLOR,vec3(shadow),vec3(0.5,0.5,0.5));//*(abs(dot(rd, cp.normal)));
         rd = refract(rd, -normal, eta);
         eta = 1./eta;
         p = cp.p;
@@ -389,7 +399,8 @@ vec3 getFloorColor(in Ray ray) {
     // Checkerboard map
     float f = mod( floor(5.0*pos.z) + floor(5.0*pos.x), 2.0);
     col = 0.4 + 0.1*f*vec3(1.0);
-    vec3 LIGHT_DIR = normalize(vec3(-0.3,1.5,-0.1));
+    //vec3 LIGHT_DIR = normalize(vec3(-0.3,1.5,-0.1));
+    vec3 LIGHT_DIR = normalize(Light.Position.xyz-pos);
     float dif = clamp( dot( ray.cp.normal, LIGHT_DIR ), 0.0, 1.0 );
     // if ray reaches the stuff inside the marble, do a different colour
     if(ray.cp.mat == ID_INSIDE)
@@ -556,7 +567,9 @@ float softShadow( in vec3 p, in vec3 dir, float maxt) {
         float h = sceneOut(p + dir*t).x;
 
         if( h < epsilon )
+        {
             return 0.0;
+        }
 
         res = min( res, (k*h)/t );
         t += h;
@@ -577,7 +590,6 @@ vec2 specular(vec3 p, vec3 n, vec3 dir,float depth)
   // Calculate the light and view vectors s and v respectively, along with the reflection vector
   vec3 s = normalize(pointToLight);
   vec3 v = normalize(pointToEye);
-  vec3 r = reflect( -s, n );
 
   // Precompute the dot products
   vec3 h = normalize(s + v);
@@ -636,102 +648,29 @@ void main() {
     dir = mat * dir;
     setScene();
 
+
     // getting values for specular
-    float depth = depthMarch(eyepos,dir);
-    vec3 p = eye + depth * dir;
-    vec3 n = normalOut(p);
+    float depthOut = depthMarchOut(eyepos,dir);
+    float depthIn = depthMarchIn(eyepos,dir);
+    vec3 pOut = eye + depthOut * dir;
+    vec3 nOut = normalOut(pOut);
+
+    vec3 pIn = eye + depthIn * dir;
+    vec3 nIn = normalIn(pIn);
 
     //get specular with NdotL, shadow and ambient
-    vec2 spec = specular(p,n,dir,depth);
-    float shadow = shadowFactor(p,n);
-    float ao = ao(p,n);
-    //fragColor = vec4( c , 1. );
+    vec2 specOut = specular(pOut,nOut,dir,depthOut);
+    vec2 specIn = specular(pIn,nIn,dir,depthIn);
+
+
+
+    float shadow = shadowFactor(pIn,nIn);
+    float ao = ao(pIn,nIn);
     vec3 c = render(eye,dir);
 
-    fragColor = vec4(0.5*ao*shadow*(Light.Ld * spec.y + Light.Ls * spec.x)+c,1.0);
+    float NdotLOverall = specIn.y;//mix(specIn.y,specOut.y,0.5);
+    float specOverall = specIn.x;//mix(specIn.x,specOut.x,0.5);
 
-    /*
-    // Initialise the scene based on the current elapsed time
-    setScene();
-    
-    // March until it hits the object. The depth indicates how far you have to travel down dir to get to the object.
-    float depth = depthMarch(eye, dir);
-    if (depth >= marchDist - epsilon) {
-        fragColor = vec4(depth);
-		return;
-    }
+    fragColor = vec4(0.5*ao*shadow*(Light.Ld * NdotLOverall + Light.Ls * specOverall)+c,1.0);
 
-    // Calculate the intersection point and the associated normal
-    vec3 p = eye + depth * dir;
-    vec3 n = normal(p);
-
-    // Shadow pass - just shoot a ray to the light
-    vec3 lightPos = Light.Position.xyz; // eye
-    vec3 pointToLight = lightPos - p;
-    float distToLight = length(lightPos - p);
-    vec3 pointToEye = eye - p;
-    
-    // Calculate the light and view vectors s and v respectively, along with the reflection vector
-    vec3 s = normalize(pointToLight);
-    vec3 v = normalize(pointToEye);
-    vec3 r = reflect( -s, n );
-
-    // Precompute the dot products
-    vec3 h = normalize(s + v);
-    float NdotL = max(dot(n, s), 0.0);
-    float NdotV = max(dot(n, v), 0.0);
-    float NdotH = max(dot(n, h), 0.0);
-    float VdotH = max(dot(v, h), 0.0);
-    
-    float specular = 0.0;
-    if (NdotL > 0.0) {
-        float mSquared = roughnessValue * roughnessValue;
-
-        // geometric attenuation
-        float NH2 = 2.0 * NdotH;
-        float invVdotH = 1.0 / VdotH;
-        float g1 = (NH2 * NdotV) * invVdotH;
-        float g2 = (NH2 * NdotL) * invVdotH;
-        float geoAtt = min(1.0, min(g1, g2));
-
-        // roughness (or: microfacet distribution function)
-        // beckmann distribution function
-        float r1 = 1.0 / ( 4.0 * mSquared * pow(NdotH, 4.0));
-        float r2 = (NdotH * NdotH - 1.0) / (mSquared * NdotH * NdotH);
-        float roughness = r1 * exp(r2);
-
-        // fresnel
-        // Schlick approximation
-        float fresnel = pow(1.0 - VdotH, 5.0);
-        fresnel *= (1.0 - F0);
-        fresnel += F0;
-
-        specular = max(0.0, (fresnel * geoAtt * roughness) / (NdotV * NdotL * 3.14));
-    }
-    
-
-        // Determine if in shadow
-    float shadowFactor = softShadow(p + n*epsilon, s, distToLight);    
-
-    // The result of this function will be a value between 0 and 1 describing how much darker this fragment should be
-    float aoFactor = ao(p, n);    
-
-    switch(colourMode) {        
-    case 2:
-        fragColor = vec4(Palette(aoFactor), 1.0);
-        break;
-    case 3:
-        //fragColor = vec4(Palette(sceneWithoutGround(p)),1.0);
-        break;
-    case 4:
-        fragColor = vec4(Palette(NdotL), 1.0);
-        break;
-    case 5:
-        fragColor = vec4(Palette(shadowFactor), 1.0);
-        break;    
-    default:    
-        fragColor = vec4(aoFactor * shadowFactor * (Light.Ld * NdotL + Light.Ls * specular), 1.0);
-        break;
-    }
-    */
 }
